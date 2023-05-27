365b3d71246f36e9f032a054835e7604dde6ca67
Jean Reis <78772097+iser-naej@users.noreply.github.com>
2023-05-27 14:44:54 -0300

    create flex-box layout and selections

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ hamburguer.addEventListener('click', () => {
   hamburguer.classList.toggle('active');
   navList.classList.toggle('active');
 });
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+  hamburguer.classList.remove('active');
+  navList.classList.remove('active');
+}))
