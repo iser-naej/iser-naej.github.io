346c2f60449195e2fe5ca938da084777bca2f4d9
Jean Reis <78772097+jeancrdo@users.noreply.github.com>
2023-06-24 17:05:21 -0300

    ajustes aparência

--- js/script.js
+++ js/script.js
@@ -10,3 +10,23 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
   hamburguer.classList.remove('active');
   navList.classList.remove('active');
 }))
+
+
+window.addEventListener('scroll', function() {
+  var elementos = document.querySelectorAll('.flutuante');
+  
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    var rect = elemento.getBoundingClientRect();
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    
+    if (rect.top <= windowHeight && rect.bottom >= 0) {
+      elemento.classList.add('flutuando');
+    } else {
+      elemento.classList.remove('flutuando');
+    }
+  }
+});
+
+
+
